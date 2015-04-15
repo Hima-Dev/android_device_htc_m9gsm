7d7324af5dbbda6984d8e8b1313dac3d49b42648
Rashed Abdel-Tawab <rashed@linux.com>
2015-04-14 21:08:20 -0400

    m9gsm: Move proprietary file scripts to m9-common

--- extract-files.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-set -e
-
-export VENDOR=htc
-export DEVICE=m9gsm
-./../../$VENDOR/m9-common/extract-files.sh $@
--- setup-makefiles.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e
-
-export VENDOR=htc
-export DEVICE=m9gsm
-./../../$VENDOR/m9-common/setup-makefiles.sh $@
